0d4f129c3bd0ece9e732b5be7ae5b2fc16835143
Albert <albert_henry@outlook.com>
2020-02-08 21:37:44 +0000

    restructure files to create a python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tofu-pkg-spiros", # Replace with your own username
+    version="0.0.1",
+    author="Spiros Denaxas",
+    author_email="s.denaxas@ucl.ac.uk",
+    description="A Python library for generating synthetic UK Biobank data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spiros/tofu.gitt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ tofu/__init__.py
@@ -0,0 +1,1 @@
+# Dummy file to transform tofu into a python package
